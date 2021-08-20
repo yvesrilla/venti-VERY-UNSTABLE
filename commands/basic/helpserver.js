e1d80981103d070587168c07cb4be007c3179271
michal! <76213511+yvesrilla@users.noreply.github.com>
2021-08-20 16:31:24 +0100

    Delete commands/basic directory

--- commands/basic/helpserver.js
+++
@@ -1,8 +0,0 @@
-const discord = require('discord.js');
-
-module.exports={
-    name:"support",
-    async run(bot, message, args){
-        message.channel.send('join our support server for venti updates and his status! https://discord.gg/HsR7WkjcDb')
-    }
-}
